6c6f0f3b00dde51f564b72abc05d0d649fd4b3a0
Davi Fernandes <davi.fer159@gmail.com>
2025-10-08 17:46:21 -0300

    fix: add write operations

--- api/server.js
+++ api/server.js
@@ -1,8 +1,13 @@
 const jsonServer = require("json-server");
+const fs = require("fs");
+const path = require("path");
 
 const server = jsonServer.create();
 
-const router = jsonServer.router("db.json");
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
 
 const middlewares = jsonServer.defaults();
 
@@ -19,4 +24,3 @@ server.listen(3000, () => {
 });
 
 module.exports = server;
-
